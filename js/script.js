d0e0f8ccad289dfce5c8c063ef432c2e9d6de14f
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2023-06-19 15:38:51 -0300

    novo

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-let nomes = ["lucas, pedro, bruno, carlos, ilton, junior, william, maria, luana, beicha, Pereira, osvaldo, vanilda, joana, gomes "]
+// let nomes = ["lucas, pedro, bruno, carlos, ilton, junior, william, maria, luana, beicha, Pereira, osvaldo, vanilda, joana, gomes "]
 
-nomes.push("Luisa", "Ricardo", "Sofia");
-nomes.splice(2, 0, "Gabriel")
-nomes.splice(6, 0, "Marina")
-nomes.splice(7, 0, "Daniel")
-nomes.sort().reverse()
-nomes.pop()
-nomes.shift()
-nomes.splice(5, 1)
-nomes.sort()
-for (let i = 0; i < nomes.length; i++) {
-    console.log(`Aluno: ${nomes[i]}, Posição: ${i}`)
-}
-console.log(nomes)
+// nomes.push("Luisa", "Ricardo", "Sofia");
+// nomes.splice(2, 0, "Gabriel")
+// nomes.splice(6, 0, "Marina")
+// nomes.splice(7, 0, "Daniel")
+// nomes.sort().reverse()
+// nomes.pop()
+// nomes.shift()
+// nomes.splice(5, 1)
+// nomes.sort()
+
+// for (let i = 0; i < nomes.length; i++) {
+//     console.log(`Aluno: ${nomes[i]}, Posição: ${i}`)
+// }
+// console.log(nomes)
+
+// let posicao = 0
+// for (const nome of nomes) {
+//     console.log(`Aluno: ${nomes}, Posição: ${posicao}`)
+//     posicao++
+// }
+
+// let notas = []
+// for (let i = 0; i < nomes.length; i++) {
+//     notas.push(Math.floor(Math.random() * 10) + 1)
+// }
+
+// notas = notas.map(nota => nota + 1.0)
+// let notasMaioresQueSete = notas.filter(nota => nota > 7)
+// console.log('Notas maiores que 7:', notasMaioresQueSete)
 
 
-let posicao = 0
-for (const nomes of alunos) {
-    console.log(`Aluno: ${nomes}, Posição: ${posicao}`)
-    posicao++
-}
-
-let notas = []
-for (let i = 0; i < nomes.length; i++) {
-    notas.push(Math.floor(Math.random() * 10) + 1)
-}
-
-notas = notas.map(nota => nota + 1.0)
-let notasMaioresQueSete = notas.filter(nota => nota > 7)
-console.log('Notas maiores que 7:', notasMaioresQueSete)
-
-
-
+//=====================================================
 
 // 1. Crie um array que armazene os nomes de 15 alunos.
 let alunos = ['João', 'Maria', 'Carlos', 'Ana', 'Pedro', 'Mariana', 'Lucas', 'Julia', 'Felipe', 'Fernanda', 'Gabriel', 'Laura', 'Rafael', 'Patrícia', 'Marcelo']
@@ -66,16 +66,40 @@ for (const aluno of alunos) {
     posicao1++
 }
 // 10. Inicialize um array vazio.
-let notas1 = [];
+let notas1 = []
 
 // 11. Utilize o Loop FOR tradicional para adicionar as notas dos alunos neste novo array.
 for (let i = 0; i < alunos.length; i++) {
-  notas1.push(Math.floor(Math.random() * 10) + 1);
+  notas1.push(Math.floor(Math.random() * 10) + 1)
 }
 
 // 12. Utilize o método map para adicionar 1.0 ponto à nota de cada aluno.
-notas = notas.map(nota => nota + 1.0);
+notas1 = notas1.map(nota => nota + 1.0)
 
 // 13. Utilize o método filter para exibir apenas as notas maiores do que 7.
-let notasMaioresQueSete1 = notas.filter(nota => nota > 7);
-console.log('Notas maiores que 7:', notasMaioresQueSete1);
+let notasMaioresQueSete1 = notas1.filter(nota => nota > 7)
+console.log('Notas maiores que 7:', notasMaioresQueSete1)
+
+//14 utilize o loop for  tradicional para percorrer os dois array nome e notas e mostre o nome e nota de cada  aluno.
+
+nomes = ["João", "Maria", "Carlos"]
+notas = [8.5, 9.0, 7.5]
+
+for (i = 0; i < nomes.length; i++) {
+    nomeAluno = nomes[i]
+    notaAluno = notas[i].toFixed(1)
+    console.log("Aluno: " + nomeAluno + " - Nota: " + notaAluno)
+}
+
+
+//15 utilie o metodo forerach para percorrer o array de notas e mostrar se cada aluno está aprovado (nota >=7.0) ou reprovado.
+
+nomes = ["João", "Maria", "Carlos"]
+notas = [8.5, 9.0, 6.5]
+
+nomes.forEach(function(nome, index) {
+    nota = notas[index]
+    situacao = nota >= 7.0 ? "Aprovado" : "Reprovado"
+    
+    console.log(nome + " - Nota: " + nota.toFixed(1) + " - " + situacao)
+})
